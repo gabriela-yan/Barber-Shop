c121077a8df16e02cef170b7ed065bd4ac7cb532
Gabriela Yan <gabriela.maldonado.yan@gmail.com>
2021-02-23 23:42:34 -0600

    Tabs functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,48 @@
+let page = 1;
+
 document.addEventListener('DOMContentLoaded', function() {
     initApp();
 })
 
 function initApp(){
     showServices();
+    // Highlight the current DIV according to the tab that is pressed
+    showSection();
+    // Hide or show a section according to the tab that is pressed
+    changeSection()
+}
+
+function showSection() {
+    const currentSection = document.querySelector(`#step-${page}`);
+    currentSection.classList.add('show-section');
+
+    // Highlight the current tab
+    const tab = document.querySelector(`[data-step="${page}"]`);
+    tab.classList.add('current');
+}
+
+function changeSection() {
+    const links = document.querySelectorAll('.tabs button');
+    links.forEach( link => {
+        link.addEventListener('click', e => {
+            e.preventDefault();
+            page = parseInt(e.target.dataset.step);
+            
+            // Remove the show-section class from the previous section
+            document.querySelector('.show-section').classList.remove('show-section');
+
+            // Add show-section where we clicked
+            const section = document.querySelector(`#step-${page}`);
+            section.classList.add('show-section');
+
+            // Delete the Current class in the previous tab
+            document.querySelector('.tabs button.current').classList.remove('current');
+            
+            // Add the Current class in the new tab
+            const tab = document.querySelector(`[data-step="${page}"]`);
+            tab.classList.add('current');
+        })
+    })
 }
 
 async function showServices(){
@@ -29,6 +68,10 @@ async function showServices(){
             const divService = document.createElement('DIV');
             divService.classList.add('service');
 
+            //Select a service for the appointment
+            divService.onclick = selectedService;
+            divService.dataset.idService = id; 
+
             // Inject price and name to divService
             divService.appendChild(nameService);
             divService.appendChild(priceService);
@@ -38,5 +81,19 @@ async function showServices(){
         })
     } catch (error) {
         console.log(error);
+    }
+}
+function selectedService(event) {
+    let element;
+    //Force the element we click to be the DIV
+    if(event.target.tagName === 'P'){ 
+        element = event.target.parentElement;
+    } else {
+        element = event.target;
+    }
+    if(element.classList.contains('selected')){
+        element.classList.remove('selected');
+    } else {
+        element.classList.add('selected');
     }
 }
